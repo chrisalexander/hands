1396aed522a461a72a1599d1b384d597f365767f
chrisalexander <chris@chris-alexander.co.uk>
2013-02-10 14:01:04 +0000

    NextPriorityGenerator generates queue priorities that always ensure one is run next

---
+++ runner/NextPriorityGenerator.py
@@ -0,0 +1,27 @@
+from threading import Lock
+
+class NextPriorityGenerator:
+    """ Generates a priority number for a job to run next.
+    
+    This encapsulates some thread-safe locking behaviour around a counter.
+    The next job to run is determined by which has the highest priority number.
+    To guarantee a "next" job is actually run next, the number must exceed all others
+    in the stack. To do this we have an incrementor which provides continuously growing
+    numbers. Due to the threaded nature of the queue execution we must make sure that
+    grabbing this number is thread-safe when multiple concurrent jobs are running.
+    
+    """
+    
+    def __init__(self, start = 1):
+        """ Initialise the generator. """
+        
+        self.next = start;
+        self.lock = Lock()
+        
+    def getNextPriority(self):
+        """ Returns the next thread priority level. """
+        self.lock.acquire()
+        nextval = self.next
+        self.next = self.next + 1
+        self.lock.release()
+        return nextval
---
+++ runner/tests/NextPriorityGeneratorTest.py
@@ -0,0 +1,60 @@
+'''
+Created on 10 Feb 2013
+
+@author: dev
+'''
+import unittest
+from runner.NextPriorityGenerator import NextPriorityGenerator
+from random import Random
+from time import sleep
+from threading import Thread
+from time import time
+
+class Test(unittest.TestCase):
+
+
+    def testBasicUsage(self):
+        r = Random()
+        n = r.randint(0, 10000)
+        npg = NextPriorityGenerator(n)
+        self.assertEqual(n, npg.getNextPriority(), "First result not valid")
+        for i in range(1, 10):
+            self.assertEqual(n+i, npg.getNextPriority(), "%sth result not valid" % i)
+
+    def testThreadedUsage(self):
+        r = Random()
+        n = r.randint(0, 10000)
+        npg = NextPriorityGenerator(n)
+        self.assertEqual(n, npg.getNextPriority(), "First result not valid")
+        
+        results = {}
+        threads = []
+
+        def storeResult():
+            sleep(r.random())
+            p = npg.getNextPriority()
+            results[p] = time()
+            
+        # Start up a load of threads    
+        for i in range(1, 10):
+            t = Thread(target=storeResult)
+            t.setDaemon(True)
+            t.start()
+            threads.append(t)
+        
+        # Wait for all the threads to be done
+        for t in threads:
+            t.join(2)
+        
+        # Check all the results are right
+        prev = None
+        for i in range(1, 10):
+            j = n + i
+            self.assertTrue(results.has_key(j), "Results array does not have correct key")
+            if prev is not None:
+                self.assertTrue(results[j] > prev)
+            prev = results[j]
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testBasicUsage']
+    unittest.main()
